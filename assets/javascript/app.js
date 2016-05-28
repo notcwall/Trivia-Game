f28d23ddc347325355ca003c37e7f0b777799772
Conor Mullen <conorseamus@gmail.com>
2016-05-28 16:23:48 -0500

    final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,7 +10,12 @@ var questionBank = [
 	new triviaQuestion("Hail to the king, baby.", "Army of Darkness", ["Scarface", "Lethal Weapon", "The Terminator"]),
 	new triviaQuestion("Get out of my way son, you're usin' my oxygen.", "One Flew Over the Cuckoo's Nest", ["Midnight Cowboy", "Crocodile Dundee", "The Untouchables"]),
 	new triviaQuestion("Oh, I'm sorry. Did I break your concentration?", "Pulp Fiction", ["Mystic River", "Men In Black", "Die Hard"]),
-	new triviaQuestion("Every man dies, not every man truly lives.", "Braveheart", ["Saving Private Ryan", "Pearl Harbor", "Troy"])
+	new triviaQuestion("Every man dies, not every man truly lives.", "Braveheart", ["Saving Private Ryan", "Pearl Harbor", "Troy"]),
+	new triviaQuestion("Life moves pretty fast. If you don't stop and look around once in a while, you could miss it.", "Ferris Bueller's Day Off", ["Big", "The Goonies", "The Lost Boys"]),
+	new triviaQuestion("Go ahead, make my day.", "Sudden Impact", ["The Good, the Bad and the Ugly", "A Fistful of Dollars", "Dirty Harry"]),
+	new triviaQuestion("Party on dudes!", "Bill & Ted's Excellent Adventure", ["Wayne's World", "Back to the Future", "Dogma"]),
+	new triviaQuestion("I'm gonna make him an offer he can't refuse.", "The Godfather", ["Goodfellas", "Scarface", "Casino"]),
+	new triviaQuestion("Are you gonna bark all day little doggy, or are you gonna bite?", "Reservoir Dogs", ["Heat", "Sin City", "The Usual Suspects"])
 ];
 
 function triviaQuestion (question, correctAnswer, incorrectAnswers){
@@ -51,9 +56,9 @@ function playGame(){
 	if(currentQuestionIndex < questionBank.length){
 		if(hasChosenAnswer == false){
 			hasChosenAnswer = true;
-			timer = setTimeout(playGame, 1000 * 4);
+			timer = setTimeout(playGame, 1000 * 3);
 			incorrectGuesses++;
-			$('#questionDisplay').html('<h2>Out of time<br>Correct Answer: ' + questionBank[currentQuestionIndex].correctAnswer);
+			$('#questionDisplay').html('<h3>Out of time<br>Correct Answer: ' + questionBank[currentQuestionIndex].correctAnswer);
 			currentQuestionIndex++;
 		}
 		else{
@@ -71,14 +76,14 @@ function playGame(){
 				$('.answerChoice').on('click', function(){
 					hasChosenAnswer = true;
 					clearTimeout(timer);
-					timer = setTimeout(playGame, 1000 * 4)
+					timer = setTimeout(playGame, 1000 * 3)
 					if($(this).text() == questionBank[currentQuestionIndex].correctAnswer){
 						correctGuesses++;
-						$('#questionDisplay').html('<h2>Correct</h1>');
+						$('#questionDisplay').html('<h3>Correct</h1>');
 					}
 					else{
 						incorrectGuesses++;
-						$('#questionDisplay').html('<h2>Incorrect<br>Correct Answer: ' + 
+						$('#questionDisplay').html('<h3>Incorrect<br>Correct Answer: ' + 
 							questionBank[currentQuestionIndex].correctAnswer);
 					}
 					currentQuestionIndex++;
