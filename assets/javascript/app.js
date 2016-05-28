c200d689354fae6be4bede0abd67671d328a7d67
Conor Mullen <conorseamus@gmail.com>
2016-05-28 04:37:05 -0500

    nearly finished, timer needs tweaking

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,15 @@
-var userGuess;
 var correctGuesses = 0;
 var incorrectGuesses = 0;
+var currentQuestionIndex = 0;
+var hasChosenAnswer = true;
+var timer;
 var questionBank = [
-	new triviaQuestion ("how old am i", "23", ["21", "22", "24"]),
-	new triviaQuestion ("test question 2", "c", ["a", "b", "d"])
+	new triviaQuestion("The Dude abides.", "The Big Lebowski", ["Airplane", "Clerks", "Top Gun"]),
+	new triviaQuestion("Hail to the king, baby.", "Army of Darkness", ["Scarface", "Lethal Weapon", "The Terminator"]),
+	new triviaQuestion("Get out of my way son, you're usin' my oxygen.", "One Flew Over the Cuckoo's Nest", ["Midnight Cowboy", "Crocodile Dundee", "The Untouchables"]),
+	new triviaQuestion("Oh, I'm sorry. Did I break your concentration?", "Pulp Fiction", ["Mystic River", "Men In Black", "Die Hard"]),
+	new triviaQuestion("Every man dies, not every man truly lives.", "Braveheart", ["Saving Private Ryan", "Pearl Harbor", "Troy"])
 ];
-
-
 
 function triviaQuestion (question, correctAnswer, incorrectAnswers){
 	this.question = question;
@@ -18,9 +21,8 @@ function triviaQuestion (question, correctAnswer, incorrectAnswers){
 		var shuffledAnswers = [];
 		var addedAnswers = [];
 
-		for(var i = 0; i < incorrectAnswers.length; i++){
+		for(var i = 0; i < incorrectAnswers.length; i++)
 			tempAnswers.push(incorrectAnswers[i]);
-		}
 
 		var tempIndex = 0;
 		while(tempIndex < tempAnswers.length){
@@ -34,5 +36,94 @@ function triviaQuestion (question, correctAnswer, incorrectAnswers){
 
 		return shuffledAnswers;
 	}
-	//this.shuffledAnswers = triviaQuestion.questionShuffler;
 }
+
+function playGame(){
+	timer = clearTimeout();
+	if(currentQuestionIndex < questionBank.length){
+		if(hasChosenAnswer == false){
+			hasChosenAnswer = true;
+			timer = clearTimeout();
+			timer = setTimeout(playGame, 1000 * 4);
+			incorrectGuesses++;
+			$('#questionDisplay').html('<h2>Out of time<br>Correct Answer: ' + questionBank[currentQuestionIndex].correctAnswer);
+			currentQuestionIndex++;
+		}
+		else{
+			timer = clearTimeout();
+			timer = setTimeout(playGame, 1000 * 10);
+			hasChosenAnswer = false;
+			if(currentQuestionIndex < questionBank.length){
+				$('#questionDisplay').html('<h1>Quote #' + (currentQuestionIndex + 1) + '</h1><br><h2>' + 
+					questionBank[currentQuestionIndex].question);
+				var currentAnswerChoices = questionBank[currentQuestionIndex].answerShuffler();
+				for(var i = 0; i < currentAnswerChoices.length; i++){
+					$('#questionDisplay').append('<button class="answerChoice">' + currentAnswerChoices[i] + '</button><br>');
+				}
+				$('.answerChoice').on('click', function(){
+					hasChosenAnswer = true;
+					timer = clearTimeout();
+					if($(this).text() == questionBank[currentQuestionIndex].correctAnswer){
+						correctGuesses++;
+						$('#questionDisplay').html('<h2>Correct</h1>');
+					}
+					else{
+						incorrectGuesses++;
+						$('#questionDisplay').html('<h2>Incorrect<br>Correct Answer: ' + 
+							questionBank[currentQuestionIndex].correctAnswer);
+					}
+					currentQuestionIndex++;
+				});
+			}
+			else{
+				timer = clearTimeout();
+				$('#questionDisplay').removeClass();
+				$('#questionDisplay').addClass('hidden');
+				$('#endDisplay').removeClass();
+				$('#endDisplay').addClass('shown');
+				$('#endDisplay').html('<h1>Finished!</h1><br><h2>Correct: ' + correctGuesses + 
+					'<br>Incorrect: ' + incorrectGuesses);
+				$('#resetButton').removeClass();
+				$('#resetButton').addClass('shown');
+			}
+		}
+	}
+	else{
+		timer = clearTimeout();
+		$('#questionDisplay').removeClass();
+		$('#questionDisplay').addClass('hidden');
+		$('#endDisplay').removeClass();
+		$('#endDisplay').addClass('shown');
+		$('#endDisplay').html('<h1>Finished!</h1><br><h2>Correct: ' + correctGuesses + 
+			'<br>Incorrect: ' + incorrectGuesses);
+		$('#resetButton').removeClass();
+		$('#resetButton').addClass('shown');
+	}
+}
+
+
+$('#startButton').on('click', function(){
+	timer = clearTimeout();
+	$('#startDisplay').removeClass();
+	$('#startDisplay').addClass('hidden');
+	$('#questionDisplay').removeClass();
+	$('#questionDisplay').addClass('shown');
+	$('#startDisplay').Hide;
+	$('#questionDisplay').Show;
+	playGame();
+});
+
+$('#resetButton').on('click', function(){
+	timer = clearTimeout();
+	$('#resetButton').removeClass();
+	$('#resetButton').addClass('hidden');
+	$('#questionDisplay').removeClass();
+	$('#questionDisplay').addClass('hidden');
+	$('#endDisplay').removeClass();
+	$('#endDisplay').addClass('hidden');
+	$('#startDisplay').removeClass();
+	$('#startDisplay').addClass('shown');
+	currentQuestionIndex = 0;
+	correctGuesses = 0;
+	incorrectGuesses = 0;
+});
